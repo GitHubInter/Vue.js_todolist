896d631cbf01f156ad8f901840a18e0ed6f80a30
GitHubInter <2686671526@qq.com>
2020-12-18 12:07:33 +0800

    add src

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './app.vue'
+
+import './assets/styles/style.css'
+import './assets/styles/test-stylus.styl'
+import './assets/images/9183.png'
+
+const root = document.createElement('div')
+document.body.appendChild(root)
+
+new Vue({
+	render: (h) => h(App)
+}).$mount(root)
